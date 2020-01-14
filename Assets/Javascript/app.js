cca717f249cc4e66b554c24c4ec8a1c6bafcc522
Mlipphardt <matthewlipphardt@gmail.com>
2020-01-14 17:49:40 -0500

    Added first question as placeholder, uses alerts currently

---
+++ Assets/Javascript/app.js
@@ -0,0 +1,18 @@
+let correctAnswers;
+let incorrectAnswers;
+
+$("#answerA").on("click", function(){
+    alert("Correct!")
+})
+
+$("#answerB").on("click", function(){
+    alert("Incorrect!")
+})
+
+$("#answerC").on("click", function(){
+    alert("Incorrect!")
+})
+
+$("#answerD").on("click", function(){
+    alert("Incorrect!")
+})
